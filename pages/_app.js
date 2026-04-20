4b74d2b841b80715747c95367477338189ae0ae5
miriamRichter94 <dark.rose2094@gmail.com>
2026-04-20 15:29:18 +0200

    Add fetcher and SWR Config

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
+import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
 
 export default function App({ Component, pageProps }) {
+  const fetcher = async (url) => {
+    const res = await fetch(url);
+    if (!res.ok) {
+      const error = new Error("An error occured while fetching the data!");
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+
+    return res.json();
+  };
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
