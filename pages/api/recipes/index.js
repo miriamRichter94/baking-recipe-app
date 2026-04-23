9e540d607af61cae11e8017c4818c87c891c2346
miriamRichter94 <dark.rose2094@gmail.com>
2026-04-23 15:29:40 +0200

    resturcturing file for unified structure over all files

--- pages/api/recipes/index.js
+++ pages/api/recipes/index.js
@@ -4,24 +4,20 @@ import Recipe from "@/db/models/recipe";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    try {
+  try {
+    if (request.method === "GET") {
       const recipes = await Recipe.find({})
         .populate("ingredients.ingredient")
         .populate("ingredients.unit");
       return response.status(200).json(recipes);
-    } catch (error) {
-      return response.status(400).json({ error: error.message });
     }
-  }
 
-  if (request.method === "POST") {
-    try {
+    if (request.method === "POST") {
       const recipe = await Recipe.create(request.body);
       return response.status(201).json(recipe);
-    } catch (error) {
-      return response.status(400).json({ error: error.message });
     }
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
   }
 
   return response.status(405).json({ stauts: "Method not allowed." });
