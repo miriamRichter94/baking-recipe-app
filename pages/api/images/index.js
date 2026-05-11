536f6a6b43ba7786ec6f08377df3ea08bbf81c1b
miriamRichter94 <dark.rose2094@gmail.com>
2026-05-11 11:43:19 +0200

    adjustments to optimize code

--- pages/api/images/index.js
+++ pages/api/images/index.js
@@ -59,7 +59,7 @@ export default async function handler(request, response) {
       return response.status(200).json(result);
     }
   } catch (error) {
-    return response.status(400).json({ error: error.message });
+    return response.status(500).json({ error: error.message });
   }
 
   return response.status(405).json({ status: "Method not allowed." });
